bf5c815668bfec9707e8fd088269cb36b4400d40
Lukas Hruby <lukas.hruby@gmail.com>
2017-06-26 21:00:40 +0200

    update versioning

--- src/main/java/cz/hrubysoftware/smsmanagerlib/SmsManager.java
+++ src/main/java/cz/hrubysoftware/smsmanagerlib/SmsManager.java
@@ -6,8 +6,6 @@ import org.apache.commons.codec.digest.DigestUtils;
 
 @Data
 public class SmsManager {
-    public static final String VERSION = "1.0";
-    public static final String JAVA_VERSION = System.getProperty("java.version");
 
     private String username;
     private String sha1password;
